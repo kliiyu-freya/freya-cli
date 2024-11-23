d0f47382e55e77c798e8eabe256700f2e2183b5d
Kliiyu <adri2007900@gmail.com>
2024-11-23 01:05:04 +0100

    mqtt broker default package; todo: reserve ipv4 for default packages

--- freya_cli/default_packages.py
+++ freya_cli/default_packages.py
@@ -6,12 +6,20 @@ core = {
     "ipv4": "192.168.168.2"
 }
 
+mqtt_broker = {
+    "name": "mqtt_broker",
+    "version": "latest",
+    "image": "eclipse-mosquitto:latest",
+    "ports": [1883, 9001],
+    "ipv4": "192.168.168.3"
+}
+
 dashboard = {
     "name": "dashboard",
     "version": "latest",
     "image": "hello-world:latest",
     "ports": [6673],
-    "ipv4": "192.168.168.3"
+    "ipv4": "192.168.168.4"
 }
 
-default_packages = [core, dashboard]
+default_packages = [core, mqtt_broker, dashboard]
